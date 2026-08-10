bda1eb64381d328524fbe939ed7d1d2f6a343892
sinhan58 <sinhan58@gmail.com>
2026-08-10 22:22:48 +0900

    Update qt-page.js

--- utils/qt-page.js
+++ utils/qt-page.js
@@ -59,6 +59,14 @@ function renderQtDetailPage({ site, item, prev, next, siteUrl }) {
 ${extraFontLinks}
 <link rel="stylesheet" href="/css/style.css" />
 ${fontStyleTag}
+
+<!-- PWA: 홈 화면에 추가했을 때 앱처럼 보이도록 하는 설정 -->
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#0d1526" />
+<link rel="icon" href="/icons/icon-192.png" />
+<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 </head>
 <body>
 
